200150a6f28cb2909ebef0b14716452e8b45b78f
grbnchrn <gorbincherian@outlook.com>
2021-07-20 15:44:56 +0530

    updating student list request

--- src/main/java/com/grbn/azure/helloword/controller/MessageController.java
+++ src/main/java/com/grbn/azure/helloword/controller/MessageController.java
@@ -1,13 +1,34 @@
 package com.grbn.azure.helloword.controller;
 
+import com.grbn.azure.helloword.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.grbn.azure.helloword.util.FakerUtil.*;
 
 @RestController
 public class MessageController {
 
-    @GetMapping("/getMessage")
-    public String getInitialMessage(){
-        return "Application deployed successfully";
+  @GetMapping("/getMessage")
+  public String getInitialMessage() {
+    return "Application deployed successfully";
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudentList(@RequestParam("count") int count) {
+    List<Student> students = new ArrayList<>();
+    if (count == 0) {
+      System.out.println("setting default value 10");
+      count = 10;
     }
+    for (int i = 0; i < count; i++) {
+      Student student = new Student(getId(), getName(), getAddress());
+      students.add(student);
+    }
+    return students;
+  }
 }
---
+++ src/main/java/com/grbn/azure/helloword/util/FakerUtil.java
@@ -0,0 +1,32 @@
+package com.grbn.azure.helloword.util;
+
+import com.grbn.azure.helloword.model.Address;
+import com.github.javafaker.Faker;
+
+public class FakerUtil {
+
+    public static final Faker faker = Faker.instance();
+
+    public static Faker getFakerInstance(){
+        return faker;
+    }
+
+    public static String getName(){
+        return faker.name().fullName();
+    }
+
+    public static Address getAddress(){
+        com.github.javafaker.Address fAdd = faker.address();
+        return new Address(fAdd.streetName(), fAdd.streetAddressNumber(),
+                fAdd.cityName(),fAdd.state(),fAdd.zipCode(),fAdd.country());
+
+    }
+
+    public static int getId(){
+        return faker.number().numberBetween(0, Integer.MAX_VALUE);
+    }
+
+
+
+
+}
