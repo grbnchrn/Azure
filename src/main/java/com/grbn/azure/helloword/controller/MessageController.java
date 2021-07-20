bd70011be3d21c0c51e1bb6a7bfe71a6654c4086
grbnchrn <gorbincherian@outlook.com>
2021-07-20 13:28:16 +0530

    updating the url

--- src/main/java/com/grbn/azure/helloword/controller/MessageController.java
+++ src/main/java/com/grbn/azure/helloword/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
-    @GetMapping("/")
+    @GetMapping("/getMessage")
     public String getInitialMessage(){
         return "Application deployed successfully";
     }
